50ed4797a375718cab05edb69021647dc5acb50d
James Troutman <jimtroutman@yahoo.com>
2024-04-08 12:03:30 -0400

    completed moviedao jdbcmoviedao movie model

---
+++ java/src/main/java/com/techelevator/dao/MovieDao.java
@@ -0,0 +1,28 @@
+package com.techelevator.dao;
+
+import com.techelevator.model.Movie;
+
+import java.util.List;
+
+public interface MovieDao {
+
+    Movie getMovieByMovieId(int id);
+
+    Movie getMovieByTitle(String title);
+
+    List<Movie> getMoviesByGenreId(int id);
+
+    List<Movie> getMoviesByUserId(int userId);
+
+    List<Movie> getMoviesByListId(int listId);
+
+    List<Movie> getMoviesByListId(List<Integer> listIds);
+
+    List<Movie> getMoviesByGenreId(List<Integer> genreIds);
+
+
+
+
+
+
+}
